a9b1ada179758f3fad5c5544f13a32cf7c1ba261
Khambhatwala Vicky Mukeshbhai <vickykhambhatwala@gmail.com>
2026-01-12 23:22:37 +0530

    Update UserRepository.java

--- OVS/src/main/java/in/vicky/main/repository/UserRepository.java
+++ OVS/src/main/java/in/vicky/main/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // This must match the method called in AuthController
+
     User findByEmail(String email); 
+
 }
